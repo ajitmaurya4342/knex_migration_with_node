e3d923725a8f5af120c217166404976739f39984
Ajit Maura <ajit@binarynumbers.io>
2021-02-14 19:50:07 +0530

    Level Added

---
+++ migrations/20210214193931_insert_game_level.js
@@ -0,0 +1,41 @@
+
+exports.up = async function (knex) {
+
+    // table.increments("level_id").primary();
+    // table.integer("game_id", 11);
+    // table.string("level_name", 45);
+    // table.integer("level", 11);
+    // table.enu("level_is_active", ["0", "1"]);//l for active 
+    // table.integer("created_by").defaultTo();
+    // table.integer("updated_by").defaultTo();
+    // table.datetime("created_at").defaultTo(knex.fn.now());
+    // table.datetime("updated_at").defaultTo(knex.fn.now());
+    let arrayGame = []
+
+    for (i = 1; i <= 3; i++) {
+        for (j = 1; j <= 50; j++) {
+            let obj = {
+                game_id: i,
+                level: j,
+                level_name: "Level" + j,
+                level_is_active: "1"
+            }
+            arrayGame.push(obj)
+        }
+
+    }
+    console.log(arrayGame)
+
+    // setTimeout(async () => {
+    await knex("m_level").insert(arrayGame).then(response => {
+        console.log("fsdfdsf", response);
+    }).catch(err => {
+        console.log("fdsfsdf");
+    })
+    // }, 1000)
+
+};
+
+exports.down = async function (knex) {
+
+};

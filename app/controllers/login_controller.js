bbd3bb690d5d812d42b039e498d05675355a75a8
Ajit Maura <ajit@binarynumbers.io>
2021-02-17 02:27:35 +0530

    Changes

--- app/controllers/login_controller.js
+++ app/controllers/login_controller.js
@@ -182,8 +182,9 @@ module.exports.getLevelList = async (req, res) => {
             });
 
 
+
             if (response.data.length == 1) {
-                global.knexCon("m_questions").whereIn("question_id", response.data[0].question_id).then(res33 => {
+                global.knexCon("m_questions").whereIn("question_id", response.data[0].question_id).orderByRaw('RAND()').then(res33 => {
                     res33.map(y => {
                         y["UserAnswer"] = ""
                     })
@@ -340,12 +341,13 @@ module.exports.getGameList = async (req, res) => {
 //User Score Score
 module.exports.addScore = async (req, res) => {
     let reqbody = req.body
-    let validateArray = ["level_id", "user_id", "level_score", "level_out_of"];
+    let validateArray = ["level_id", "user_id", "level_out_of"];
     let responseError = await CheckValidation(validateArray, reqbody);
     if (responseError.status) {
 
         let settingArray = await global.knexCon("settings");
         let UserArray = await global.knexCon("m_user").where({ user_id: reqbody.user_id });
+        let LevelArray = await global.knexCon("m_level").where({ level_id: reqbody.level_id });
         let checkExamForLevel = await global.knexCon("user_level_score").where({ user_id: reqbody.user_id, level_id: reqbody.level_id, score_is_active: "1" });
         let obj = {
             level_id: reqbody.level_id,
@@ -353,19 +355,23 @@ module.exports.addScore = async (req, res) => {
             level_score: reqbody.level_score,
             level_out_of: reqbody.level_out_of,
         }
-        let msg = ""
+        let msg = "";
+        let sendArrayStar = [];
+        let passedStatus = false;
         console.log(checkExamForLevel, "dsad");
 
         // if (checkExamForLevel.length == 0) {
 
         let updatePoints = 0;
+
         if (parseInt(settingArray[0].passing_score) <= (reqbody.level_score)) {
-            msg = "Level Completed";
+            msg = "Level " + LevelArray[0].level + " Completed";
+            passedStatus = true
             updatePoints = parseInt(UserArray[0].user_points) + parseInt(settingArray[0].credit_pass_points);
             obj["score_is_active"] = "1"
         } else {
             obj["score_is_active"] = "0"
-            msg = "Level Failed";
+            msg = "Level " + LevelArray[0].level + " Failed";
             updatePoints = parseInt(UserArray[0].user_points) - parseInt(settingArray[0].level_fail_dedution_point);
         }
         if (updatePoints < 0) {
@@ -381,15 +387,49 @@ module.exports.addScore = async (req, res) => {
 
         }
 
+        let nextLevelId = 0
+
+        if (passedStatus) {
+            nextLevelId = await global.knexCon("m_level").where({ level: LevelArray[0].level + 1, game_id: LevelArray[0].game_id })
+            nextLevelId = nextLevelId[0].level + "-" + nextLevelId[0].level_id
+        }
+
+        let percentageCalculate = (parseInt(reqbody.level_score) / parseInt(reqbody.level_out_of)) * 100;
+        console.log(percentageCalculate, "fsfsdf");
+
+        if (percentageCalculate == 0) {
+            sendArrayStar = ["star-o", "star-o", "star-o"]
+        } else if (percentageCalculate > 0 && percentageCalculate < 33) {
+            sendArrayStar = ["star-half-o", "star-o", "star-o"]
+        } else if (percentageCalculate >= 33 && percentageCalculate < 49) {
+            sendArrayStar = ["star", "star-o", "star-o"]
+
+        } else if (percentageCalculate >= 49 && percentageCalculate < 66) {
+            sendArrayStar = ["star", "star-half-o", "star-o"]
+
+        } else if (percentageCalculate >= 66 && percentageCalculate < 82) {
+            sendArrayStar = ["star", "star", "star-o"]
+
+        } else if (percentageCalculate >= 82 && percentageCalculate < 100) {
+            sendArrayStar = ["star", "star", "star-half-o"];
+        } else {
+            sendArrayStar = ["star", "star", "star"]
+
+        }
+
+
+
 
         global.knexCon("user_level_score").insert(obj).then(response => {
             if (response.length > 0) {
+
+                UserArray[0].user_points = updatePoints
 
 
                 if (checkExamForLevel.length == 0) {
                     global.knexCon("m_user").update({ user_points: updatePoints }).where({ user_id: reqbody.user_id }).then(resUser => {
                         res.send({
-                            status: true, "Record": obj, msg: msg,
+                            status: true, "Record": obj, msg: msg, userArray: UserArray, passedStatus: passedStatus, starCheck: sendArrayStar, nextLevel: nextLevelId
                         })
 
                     }).catch(err2 => {
@@ -399,7 +439,7 @@ module.exports.addScore = async (req, res) => {
 
                 } else {
                     res.send({
-                        status: true, "Record": obj, msg: msg,
+                        status: true, "Record": obj, msg: msg, userArray: UserArray, passedStatus: passedStatus, starCheck: sendArrayStar, nextLevel: nextLevelId
                     })
 
                 }
@@ -556,18 +596,36 @@ module.exports.getuserlist = async (req, res) => {
 //app api 
 
 module.exports.getLevelByGame = async (req, res) => {
+    let game_level = await global.knexCon.raw(`select m_level.*,level_score,if(level_score is null,0,(level_score/level_out_of)*100) as percentage from m_level left join user_level_score on user_level_score.level_id=m_level.level_id and user_level_score.score_is_active='1' and user_level_score.user_id="${req.query.user_id}" where game_id=${req.query.game_id}
+    `);
 
-    let game_level = await global.knexCon("m_level").where((builder) => {
+    let user_level = await global.knexCon("user_level_score").select("m_level.level").join("m_level", "m_level.level_id", "=", "user_level_score.level_id").where({ "user_level_score.user_id": req.query.user_id, "score_is_active": "1", game_id: req.query.game_id }).orderBy(" m_level.level", "DESC").limit(1);
 
-        if (req.query.game_id) {
-            builder.where({ game_id: req.query.game_id });
+    game_level[0].map(x => {
+        if (x["percentage"] == 0) {
+            x["star_array"] = ["star-o", "star-o", "star-o"]
+        } else if (x["percentage"] > 0 && x["percentage"] < 33) {
+            x["star_array"] = ["star-half-o", "star-o", "star-o"]
+        } else if (x["percentage"] >= 33 && x["percentage"] < 49) {
+            x["star_array"] = ["star", "star-o", "star-o"]
+
+        } else if (x["percentage"] >= 49 && x["percentage"] < 66) {
+            x["star_array"] = ["star", "star-half-o", "star-o"]
+
+        } else if (x["percentage"] >= 66 && x["percentage"] < 82) {
+            x["star_array"] = ["star", "star", "star-o"]
+
+        } else if (x["percentage"] >= 82 && x["percentage"] < 100) {
+            x["star_array"] = ["star", "star", "star-half-o"];
+        } else {
+            x["star_array"] = ["star", "star", "star"]
+
         }
-    });
 
-    let user_level = await global.knexCon("user_level_score").select("m_level.level").join("m_level", "m_level.level_id", "=", "user_level_score.level_id").where({ "user_level_score.user_id": req.query.user_id, "score_is_active": "1", game_id: req.query.game_id }).orderBy(" m_level.level").limit(1);
+    })
 
     res.send({
-        status: true, "Record": game_level, msg: "Game Level", current_user_level: user_level.length > 0 ? user_level[0].level + 1 : 1
+        status: true, "Record": game_level[0], msg: "Game Level", current_user_level: user_level.length > 0 ? user_level[0].level + 1 : 1
     })
 
 }
